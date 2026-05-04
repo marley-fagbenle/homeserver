af7084405057fe1bb4036e906f4824c01353632c
marley-fagbenle <marleyolafagbenle@gmail.com>
2026-05-04 12:24:12 -0500

    Testing final CD pipeline

--- week4-webapp/app.py
+++ week4-webapp/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return "<h1>Hello from WSL!</h1><p>My Week 4 docker container is running successfully.</p>"
+	return "<h1>Week 6: Automated pipeline check.</h1><p>The robot did my job for me!</p>"
 
 if __name__ == '__main__':
 	# Bind to 0.0.0.0 so it is accessible outside the container
